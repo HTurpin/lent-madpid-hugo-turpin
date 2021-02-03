e1f1de10ba04dfb22ca867d1bae08870c890cf00
HTurpin <77404050+HTurpin@users.noreply.github.com>
2021-02-03 17:25:23 +0000

    Update pxt.json, main.blocks, main.ts, tilemap.g.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -18,10 +18,20 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         . . . . . . . . . . . . . . . . 
         `, mySprite, 200, 0)
 })
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    status.spriteAttachedTo().destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -25
+    info.changeScoreBy(1)
+})
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
-    otherSprite.destroy()
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(4, 500)
 })
+let statusbar: StatusBarSprite = null
 let Enemy_Ship: Sprite = null
 let projectile: Sprite = null
 let mySprite: Sprite = null
@@ -44,10 +54,18 @@ mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(mySprite)
+controller.moveSprite(mySprite, 100, 100)
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
-info.setLife(3)
-game.onUpdateInterval(2000, function () {
+let Enemy_Speed = 20
+info.setLife(5)
+let Enemy_Spawn_Time = 2000
+game.onUpdateInterval(5000, function () {
+    Enemy_Speed += 5
+    Enemy_Speed = Math.min(Enemy_Speed, 50)
+    Enemy_Spawn_Time += -250
+    Enemy_Spawn_Time = Math.max(Enemy_Spawn_Time, 500)
+})
+forever(function () {
     Enemy_Ship = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . . . . . . . . . . . . . 
@@ -67,6 +85,10 @@ game.onUpdateInterval(2000, function () {
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Enemy)
     Enemy_Ship.x = scene.screenWidth()
-    Enemy_Ship.vx = -20
+    Enemy_Ship.vx = 0 - Enemy_Speed
     Enemy_Ship.y = randint(10, scene.screenHeight() - 5)
+    statusbar = statusbars.create(15, 2, StatusBarKind.EnemyHealth)
+    statusbar.setColor(5, 10)
+    statusbar.attachToSprite(Enemy_Ship)
+    pause(Enemy_Spawn_Time)
 })
